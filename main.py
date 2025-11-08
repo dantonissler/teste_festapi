e7c45325e1264cf057ec7db2a07d8a85da1b3ac5
Danton Issler Rodrigues <danton.issler18@gmail.com>
2025-11-08 13:41:00 -0400

    teste

--- main.py
+++ main.py
@@ -12,6 +12,3 @@ async def webhook(req: Request):
 @app.get("/")
 def root():
     return {"msg": "Webhook ativo 🚀"}
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
